24c6002250093745b77139f9647ccab17d43c2ff
Jett <142007881+MilknCookys@users.noreply.github.com>
2024-07-17 09:07:46 +1000

    Added JS functionality to get form input values.

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ const myLibrary = [];
 function book() {}
 
 function addBookToLibrary() {
-  const title = document.querySelector("#title");
-  const author = a;
+  const title = document.querySelector("#title").value;
+
+  const author = document.querySelector("#author").value;
+
+  const page = document.querySelector("#page").value;
+
+  const read = document.querySelector(".readCheck:checked").value;
+
+  console.log(title);
+  console.log(author);
+  console.log(page);
+  console.log(read);
 }
 
 const submit = document.querySelector("#submit");
 
 submit.addEventListener("click", () => {
   console.log("You have clicked submit.");
+  addBookToLibrary();
   event.preventDefault();
 });
