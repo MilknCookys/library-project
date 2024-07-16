29c756dfd368741cb6b08cbb4087582902979793
Jett <142007881+MilknCookys@users.noreply.github.com>
2024-07-17 08:53:28 +1000

    Initial commit, forgor to make a repo to start. Basic HTML layout completed.

---
+++ index.js
@@ -0,0 +1,15 @@
+const myLibrary = [];
+
+function book() {}
+
+function addBookToLibrary() {
+  const title = document.querySelector("#title");
+  const author = a;
+}
+
+const submit = document.querySelector("#submit");
+
+submit.addEventListener("click", () => {
+  console.log("You have clicked submit.");
+  event.preventDefault();
+});
