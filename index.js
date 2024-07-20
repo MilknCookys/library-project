2da76fcd5c6b225dcdc19d255fa059ca748e9374
Jett <142007881+MilknCookys@users.noreply.github.com>
2024-07-20 23:24:08 +1000

    Basic library done.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function Book(title, author, page, read) {
   this.author = author;
   this.page = page;
   this.read = read;
+
+  this.displayed = false;
 }
 
 function addBookToLibrary() {
@@ -14,17 +16,73 @@ function addBookToLibrary() {
 
   const page = document.querySelector("#page").value;
 
-  const read = document.querySelector(".readCheck:checked").value;
+  const read =
+    document.querySelector(".readCheck:checked").value === "true"
+      ? true
+      : false;
 
   const newBook = new Book(title, author, page, read);
   myLibrary.push(newBook);
+
+  const form = document.querySelector("form");
+  form.reset();
+}
+
+function cardDisplay(library) {
+  const libraryContainer = document.querySelector("#libraryContainer");
+  for (let i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].displayed === true) {
+      continue;
+    } else {
+      const libraryCard = document.createElement("div");
+      libraryCard.setAttribute("data-id", i);
+      libraryContainer.appendChild(libraryCard);
+
+      const titleDisplay = document.createElement("p");
+      const authorDisplay = document.createElement("p");
+      const pageDisplay = document.createElement("p");
+      const readDisplay = document.createElement("p");
+      const readButton = document.createElement("button");
+
+      titleDisplay.textContent = `Title: ${myLibrary[i].title}`;
+      authorDisplay.textContent = `Author: ${myLibrary[i].author}`;
+      pageDisplay.textContent = `Page: ${myLibrary[i].page}`;
+      if (myLibrary[i].read === false) {
+        readDisplay.textContent = `Read: No`;
+      } else {
+        readDisplay.textContent = `Read: Yes`;
+      }
+      readButton.textContent = `Read`;
+
+      readButton.addEventListener("click", (event) => {
+        console.log(i);
+        if (myLibrary[i].read === true) {
+          myLibrary[i].read = false;
+        } else if (myLibrary[i].read === false) {
+          myLibrary[i].read = true;
+        }
+        console.log(myLibrary[i].read);
+        console.log(myLibrary[i]);
+        cardDisplay(myLibrary);
+      });
+
+      libraryCard.appendChild(titleDisplay);
+      libraryCard.appendChild(authorDisplay);
+      libraryCard.appendChild(pageDisplay);
+      libraryCard.appendChild(readDisplay);
+      libraryCard.appendChild(readButton);
+
+      myLibrary[i].displayed = true;
+    }
+  }
 }
 
 const submit = document.querySelector("#submit");
 
-submit.addEventListener("click", () => {
+submit.addEventListener("click", (event) => {
   console.log("You have clicked submit.");
   addBookToLibrary();
   event.preventDefault();
   console.log(myLibrary);
+  cardDisplay(myLibrary);
 });
