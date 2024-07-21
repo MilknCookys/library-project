196850a40e3bb6e9b5b3c4972306c22fd60a19b3
Jett <142007881+MilknCookys@users.noreply.github.com>
2024-07-21 21:21:05 +1000

    Everything in a functional state. Cleaning up code now.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const myLibrary = [];
+let bookIdentifier = 0;
+
+function generateBookId() {
+  return bookIdentifier++;
+}
 
 function Book(title, author, page, read) {
+  this.id = generateBookId();
+
   this.title = title;
   this.author = author;
   this.page = page;
@@ -28,14 +35,14 @@ function addBookToLibrary() {
   form.reset();
 }
 
-function cardDisplay(library) {
+function renderLibrary(library) {
   const libraryContainer = document.querySelector("#libraryContainer");
   for (let i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i].displayed === true) {
       continue;
     } else {
       const libraryCard = document.createElement("div");
-      libraryCard.setAttribute("data-id", i);
+      libraryCard.setAttribute("data-id", myLibrary[i].id);
       libraryContainer.appendChild(libraryCard);
 
       const titleDisplay = document.createElement("p");
@@ -44,24 +51,29 @@ function cardDisplay(library) {
       const readDisplay = document.createElement("p");
       readDisplay.className = "readDisplay";
       const readButton = document.createElement("button");
+      const removeButton = document.createElement("button");
 
       titleDisplay.textContent = `Title: ${myLibrary[i].title}`;
       authorDisplay.textContent = `Author: ${myLibrary[i].author}`;
       pageDisplay.textContent = `Page: ${myLibrary[i].page}`;
+
       if (myLibrary[i].read === false) {
         readDisplay.textContent = `Read: No`;
       } else {
         readDisplay.textContent = `Read: Yes`;
       }
-      readButton.textContent = `Read`;
-
+      readButton.textContent = "Read";
       readButton.addEventListener("click", onClickReadButton);
+
+      removeButton.textContent = "Remove";
+      removeButton.addEventListener("click", onClickRemoveButton);
 
       libraryCard.appendChild(titleDisplay);
       libraryCard.appendChild(authorDisplay);
       libraryCard.appendChild(pageDisplay);
       libraryCard.appendChild(readDisplay);
       libraryCard.appendChild(readButton);
+      libraryCard.appendChild(removeButton);
 
       myLibrary[i].displayed = true;
     }
@@ -75,28 +87,51 @@ submit.addEventListener("click", (event) => {
   addBookToLibrary();
   event.preventDefault();
   console.log(myLibrary);
-  cardDisplay(myLibrary);
+  renderLibrary(myLibrary);
 });
 
 function onClickReadButton(event) {
   const libraryCard = event.target.parentElement;
   const readDisplay = libraryCard.querySelector(".readDisplay");
   const bookId = Number(libraryCard.getAttribute("data-id"));
-  myLibrary[bookId].read = !myLibrary[bookId].read;
-  readDisplay.textContent = myLibrary[bookId].read ? "Read: Yes" : "Read: No";
-
-  // console.log(myLibrary[bookId]);
-  // cardDisplay(myLibrary);
-  /* console.log(i);
-  if (myLibrary[i].read === true) {
-    myLibrary[i].read = false;
-  } else if (myLibrary[i].read === false) {
-    myLibrary[i].read = true;
+  const bookIndex = getBookIndex(bookId);
+  if (bookIndex !== -1) {
+    myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
+    readDisplay.textContent = myLibrary[bookIndex].read
+      ? "Read: Yes"
+      : "Read: No";
   }
-  console.log(myLibrary[i].read);
-  console.log(myLibrary[i]);
-  cardDisplay(myLibrary);
-  */
+}
+
+function onClickRemoveButton(event) {
+  const libraryCard = event.target.parentElement;
+  const bookId = Number(libraryCard.getAttribute("data-id"));
+
+  removeFromLibrary(bookId);
+
+  libraryCard.remove();
+}
+
+function removeFromLibrary(bookId) {
+  if (getBookIndex(bookId) !== -1) {
+    myLibrary.splice(getBookIndex(bookId), 1);
+  }
+}
+
+/**
+ * Return the index of the book with the specified book id in the library.
+ *
+ * @param {number} bookId The book id of the sought book.
+ * @return {number} the index of the book with the specified book id in the library or -1 if no matching book is found.
+ */
+function getBookIndex(bookId) {
+  for (i = 0; i < myLibrary.length; i++) {
+    if (myLibrary[i].id === bookId) {
+      return i;
+    }
+  }
+  console.log("Could not find book");
+  return -1;
 }
 
 function renderCard(book) {
